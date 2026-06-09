fa4ef988b322afacdbfc8cedb7c98854446cbdb9
DoCarrie <carrie990129@gmail.com>
2026-06-09 23:16:49 +0900

    pandas-series

--- 3.pandas.py
+++ 3.pandas.py
@@ -14,4 +14,45 @@ import pandas as pd
 #시리즈 생성
 s1 = pd.Series([1,2,3], index=["A","B","C"])
 print(s1)
+'''
+인텍스 데이터
+A    1
+B    2
+C    3
+dtype: int64
+이처럼 시리즈의 데이터는 ndarray이므로 모든 요소의 자료형이 같게 설정됨
+시리즈의 자료형은 astype 메서드를 사용해서 변환
+'''
 
+s1 = s1.astype(float)
+print(s1)
+'''
+인텍스 데이터
+A    1.0
+B    2.0
+C    3.0
+dtype: float64
+'''
+
+#시리즈 - 인덱스를 생성하지 않으면 range로 자동으로 설정
+s2 = pd.Series([10,20,30])
+print(s2)
+'''
+인텍스 데이터
+0    10
+1    20
+2    30
+dtype: int64
+'''
+
+#딕셔너리를 이용한 시리즈 생성
+s3 = pd.Series({"A":1, "B":2, "C":3, "D":4})
+print(s3)
+'''
+인텍스 데이터
+A    1
+B    2
+C    3
+D    4
+dtype: int64
+'''
